ab26c192ec47abf6fafe24294a058cad64426260
spektra2147 <spektra2147@gmail.com>
2023-11-08 13:12:19 +0300

    updated this server manage && updated phpVersion

--- src/Handler/PhpVersions.php
+++ src/Handler/PhpVersions.php
@@ -8,11 +8,15 @@ class PhpVersions
 {
     public function handle(SelectFieldType $fieldType)
     {
-        $fieldType->setOptions([
+        $fieldType->setOptions($this->getValues());
+    }
+
+    public function getValues(){
+        return [
             '7.4' => 'PHP 7.4',
             '8.0' => 'PHP 8.0',
             '8.1' => 'PHP 8.1',
             '8.2' => 'PHP 8.2',
-        ]);
+        ];
     }
 }
--- src/Server/Table/ServerTableButtons.php
+++ src/Server/Table/ServerTableButtons.php
@@ -9,19 +9,23 @@ class ServerTableButtons
         $builder->setButtons([
             'action' => [
                 'text' => function (EntryInterface $entry) {
-                    return $entry->getStatus() ? 'module::button.manage': 'module::button.install';
+                    return $entry->getStatus() || $entry->getIp() == $_SERVER['REMOTE_ADDR'] ? 'module::button.manage' : 'module::button.install';
                 },
                 'type' => function (EntryInterface $entry) {
-                    return $entry->getStatus() ? 'info': 'success';
+                    return $entry->getStatus() || $entry->getIp() == $_SERVER['REMOTE_ADDR'] ? 'info' : 'success';
                 },
                 'href' => function (EntryInterface $entry) {
-                    return $entry->getStatus() ? '/admin/server/manage/{entry.server_id}': '/admin/server/installation/{entry.server_id}';
+                    return $entry->getStatus() || $entry->getIp() == $_SERVER['REMOTE_ADDR'] ? '/admin/server/manage/{entry.server_id}' : '/admin/server/installation/{entry.server_id}';
                 },
                 'icon' => function (EntryInterface $entry) {
-                    return $entry->getStatus() ? 'fa fa-cog': 'fa fa-upload';
+                    return $entry->getStatus() ? 'fa fa-cog' : 'fa fa-upload';
                 }
             ],
-            'delete'
+            'delete' => [
+                'enabled' => function (EntryInterface $entry) {
+                    return $entry->getIp() != $_SERVER['REMOTE_ADDR'];
+                },
+            ]
         ]);
     }
 }
